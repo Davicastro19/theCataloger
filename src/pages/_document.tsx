6627d76b6954d09f3be09ab3965ba0e76164abab
DAVI CASTRO FERREIRA <48842971+Davicastro19@users.noreply.github.com>
2023-09-21 13:47:09 -0300

    up

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,6 +2,7 @@ import { ColorModeScript } from "@chakra-ui/react";
 import { Html, Head, Main, NextScript } from "next/document";
 import { theme } from "../../styles/styles";
 import Script from "next/script";
+import AnalyticsHJ from "../components/AnalyticsHJ";
 //import { ColorModeScript } from '@chakra-ui/react'
 
 
@@ -10,17 +11,8 @@ export default function Document(){
         <Html>
             <Head>
             <meta charSet="utf-8" />
-            <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-          ga('create', 'G-N8M98E6D0T', 'auto');
-          ga('send', 'pageview');
-        `}
-      </Script>
+             <AnalyticsHJ/>
+             <link rel="icon" href="/favicon.ico" />
             </Head>
             <body>
             <ColorModeScript initialColorMode={theme.config.initialColorMode} />
